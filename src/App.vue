dda88575efb98d1f170b1e74781f33594fb1b52f
Jack N <jacknagle22@gmail.com>
2018-01-21 09:08:02 +1100

    First commit, basic vue application with materialize

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <app-navbar/>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+  import AppNavbar from './components/AppNavbar.vue';
+  export default {
+    name: 'app',
+    components: {
+      AppNavbar,
+    },
+  };
+
+</script>
---
+++ src/components/pages/Home.vue
@@ -0,0 +1,8 @@
+<template>
+  <div id="home">
+      <h1>Home!</h1>
+  </div>
+</template>
+
+<script>export default { name: 'home' };
+</script>
