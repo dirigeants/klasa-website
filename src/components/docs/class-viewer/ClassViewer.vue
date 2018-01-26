63bace0e2fc38bbde304055ecd16d6705229f8f2
Unknown <bdistin@gmail.com>
2018-01-26 15:29:16 -0600

    restore overview

--- src/components/docs/class-viewer/ClassViewer.vue
+++ src/components/docs/class-viewer/ClassViewer.vue
@@ -2,42 +2,36 @@
   <div v-if="clarse">
     <heading :clarse="clarse" :docs="docs" />
 
-    <h2 class="title is-3" v-if="properties && properties.length">Properties</h2>
+    <overview :properties="properties" :methods="methods" :events="clarse.events" />
+
+    <h5 class="title is-4" v-if="properties && properties.length">Properties</h5>
     <property v-for="prop in properties" :prop="prop" :docs="docs" :key="prop" />
 
-    <h2 class="title is-3" v-if="methods && methods.length">Methods</h2>
+    <h5 class="title is-4" v-if="methods && methods.length">Methods</h5>
     <method v-for="method in methods" :method="method" :docs="docs" :key="method" />
 
-    <h2 class="title is-3" v-if="clarse.events && clarse.events.length">Events</h2>
+    <h5 class="title is-4" v-if="clarse.events && clarse.events.length">Events</h5>
     <event v-for="event in clarse.events" :event="event" :docs="docs" :key="event" />
   </div>
   <unknown-page v-else class="docs-page" />
 </template>
 
 <script>
-  import TypeLink from '../TypeLink.vue';
-  import ParamTable from './ParamTable.vue';
   import Heading from './Heading';
   import Overview from './Overview';
   import Property from './Property';
   import Method from './Method';
   import Event from './Event';
-  import SourceButton from '../SourceButton.vue';
-  import See from '../See';
 
   export default {
     name: 'class-viewer',
     props: ['docs', 'showPrivate'],
     components: {
-      TypeLink,
-      ParamTable,
       Heading,
       Overview,
       Property,
       Method,
       Event,
-      SourceButton,
-      See,
     },
 
     data() {
