5dfabb9fde4a41bff0f3763a914e6bc53415e916
Unknown <bdistin@gmail.com>
2018-01-26 11:32:05 -0600

    add some overview pretties

--- src/components/docs/class-viewer/Overview.vue
+++ src/components/docs/class-viewer/Overview.vue
@@ -1,45 +1,45 @@
 <template>
-  <div id="class-overview">
-    <div class="col" v-if="properties && properties.length > 0">
+  <div class="columns">
+    <div class="column" v-if="properties && properties.length">
       <div class="title">Properties</div>
       <transition-group name="animated-list" tag="ul">
         <li v-for="property in properties" :key="scopedName(property)" @click="scroll(scopedName(property))" class="animated-list-item">
           <router-link :to="{ name: 'docs-class', query: { scrollTo: scopedName(property) } }">
             {{ property.name }}
-            <span v-if="property.scope === 'static'" class="small-badge">S</span>
-            <span v-if="property.abstract" class="small-badge">A</span>
-            <span v-if="property.deprecated" class="small-badge warn">D</span>
-            <span v-if="property.access === 'private'" class="small-badge warn">P</span>
+            <span class="tag is-primary is-pulled-right" v-if="property.scope === 'static'">S</span>
+            <span class="tag is-info is-pulled-right" v-if="property.abstract">A</span>
+            <span class="tag is-danger is-pulled-right" v-if="property.deprecated">D</span>
+            <span class="tag is-warning is-pulled-right" v-if="property.access === 'private'">P</span>
           </router-link>
         </li>
       </transition-group>
     </div>
 
-    <div class="col" v-if="methods && methods.length > 0">
+    <div class="column" v-if="methods && methods.length > 0">
       <div class="title">Methods</div>
       <transition-group name="animated-list" tag="ul">
         <li v-for="method in methods" :key="scopedName(method)" @click="scroll(scopedName(method))" class="animated-list-item">
           <router-link :to="{ name: 'docs-class', query: { scrollTo: scopedName(method) } }">
             {{ method.name }}
-            <span v-if="method.scope === 'static'" class="small-badge">S</span>
-            <span v-if="method.abstract" class="small-badge">A</span>
-            <span v-if="method.deprecated" class="small-badge warn">D</span>
-            <span v-if="method.access === 'private'" class="small-badge warn">P</span>
+            <span class="tag is-primary is-pulled-right" v-if="method.scope === 'static'">S</span>
+            <span class="tag is-info is-pulled-right" v-if="method.abstract">A</span>
+            <span class="tag is-danger is-pulled-right" v-if="method.deprecated">D</span>
+            <span class="tag is-warning is-pulled-right" v-if="method.access === 'private'">P</span>
           </router-link>
         </li>
       </transition-group>
     </div>
 
-    <div class="col" v-if="events && events.length > 0">
+    <div class="column" v-if="events && events.length > 0">
       <div class="title">Events</div>
-      <ul>
-        <li v-for="event in events" @click="scroll(event.name)">
+      <transition-group name="animated-list" tag="ul">
+        <li v-for="event in events" :key="scopedName(event)" @click="scroll(event.name)">
           <router-link :to="{ name: 'docs-class', query: { scrollTo: event.name } }">
             {{ event.name }}
-            <span v-if="event.deprecated" class="small-badge warn">D</span>
+            <span class="tag is-danger is-pulled-right" v-if="event.deprecated">D</span>
           </router-link>
         </li>
-      </ul>
+      </transition-group>
     </div>
   </div>
 </template>
