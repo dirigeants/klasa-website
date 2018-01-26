49140bdc8e9d2d7ad4d11569a7fc9b851ed4611f
Unknown <bdistin@gmail.com>
2018-01-26 10:00:01 -0600

    Fix hljs in marked

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
-import marked from 'marked';
+import marked, { Renderer } from 'marked';
 import VueHighlightJS from 'vue-highlightjs';
 import highlightjs from 'highlight.js';
 import Buefy from 'buefy';
@@ -27,9 +27,19 @@ Vue.use(Buefy, {
   defaultIconPack: 'fa'
 });
 
-marked.setOptions({
-  highlight: code => highlightjs.highlightAuto(code).value
-});
+// Create your custom renderer.
+const renderer = new Renderer();
+renderer.code = (code, language) => {
+  // Check whether the given language is valid for highlight.js.
+  const validLang = !!(language && highlightjs.getLanguage(language));
+  // Highlight only if the language is valid.
+  const highlighted = validLang ? highlightjs.highlight(language, code).value : code;
+  // Render the highlighted code with `hljs` class.
+  return `<pre><code class="hljs ${language}">${highlighted}</code></pre>`;
+};
+
+// Set the renderer to marked.
+marked.setOptions({ renderer });
 
 Vue.filter('marked', text => {
   if (!text) text = '**Documentation missing.**';
