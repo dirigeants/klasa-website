83460f94c114c3b86d594d1bc6a13a449aa1886a
Unknown <bdistin@gmail.com>
2018-01-26 23:12:13 -0600

    update build config?

--- config/index.js
+++ config/index.js
@@ -50,7 +50,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: '/klasa-website/',
 
     /**
      * Source Maps
