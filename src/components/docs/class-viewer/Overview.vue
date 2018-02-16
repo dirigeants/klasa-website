8d6230c645c13f10b3126ad97bdbb3cf05c84c5e
Unknown <bdistin@gmail.com>
2018-02-16 11:35:04 -0600

    Add async support, and fix abstract method tags

--- src/components/docs/class-viewer/Overview.vue
+++ src/components/docs/class-viewer/Overview.vue
@@ -26,10 +26,11 @@
 						<li v-for="method in methods" :key="scopedName(method)" @click="scroll(scopedName(method))" class="animated-list-item">
 							<router-link :to="{ name: 'docs-class', query: { scrollTo: scopedName(method) } }">
 								{{ method.name }}
+								<span v-if="method.access === 'private'"><span class="tag is-warning is-pulled-right">P</span>&nbsp;</span>
 								<span v-if="method.scope === 'static'"><span class="tag is-primary is-pulled-right">S</span>&nbsp;</span>
+								<span v-if="method.async"><span class="tag is-success is-pulled-right">A</span>&nbsp;</span>
 								<span v-if="method.abstract"><span class="tag is-info is-pulled-right">A</span>&nbsp;</span>
 								<span v-if="method.deprecated"><span class="tag is-danger is-pulled-right">D</span>&nbsp;</span>
-								<span v-if="method.access === 'private'"><span class="tag is-warning is-pulled-right">P</span>&nbsp;</span>
 								<span v-if="method.generator"><span class="tag is-dark is-pulled-right">G</span>&nbsp;</span>
 							</router-link>
 						</li>
