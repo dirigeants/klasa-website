80d85b95b7341e652f65be21e056579368e3581d
Unknown <bdistin@gmail.com>
2018-04-03 10:39:39 -0500

    Bring back folders

--- poi.config.js
+++ poi.config.js
@@ -1,4 +1,10 @@
 module.exports = {
+	filename: {
+		js: 'static/js/[name].js',
+		chunk: 'static/js/chunk/[id].chunk.js',
+    	css: 'static/css/[name].css'
+	},
+	copy: { from: 'assets', to: 'static' },
 	html: {
 		template: "./index.html"
 	},
