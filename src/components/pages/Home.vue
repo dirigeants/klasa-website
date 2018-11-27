4a60aba0065286036d794a68660c39bab4e483d2
Kyra <kyradiscord@gmail.com>
2018-11-27 23:55:27 +0100

    typo: ect -> etc

--- src/components/pages/Home.vue
+++ src/components/pages/Home.vue
@@ -40,7 +40,7 @@
 					<article class="tile is-child box content has-text-left">
 						<h5><strong>Key Features:</strong></h5>
 						<ul>
-							<li>OOP and completely hot-reloadable (all commands, events, inhibitors, ect).</li>
+							<li>OOP and completely hot-reloadable (all commands, events, inhibitors, etc).</li>
 							<li>Works with any database.</li>
 							<li>Powerful and easy to configure permission levels system.</li>
 							<li>Per Client/Guild/User settings, and possible to add more.</li>
